abf25ed6df8af7023719cbb9b7c9a9b2b8b82321
Tymofii <timka.nikolaev15@gmail.com>
2023-12-20 11:48:20 +0100

    V0.1

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    private int seconds = 0;
+
+    // Is the stopwatch running?
+    private boolean running;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        runTimer();
+    }
+
+    public void onClickStart(View view)
+    {
+        running = true;
+    }
+
+    public void onClickStop(View view)
+    {
+        running = false;
+    }
+    private void runTimer()
+    {
+
+        // Get the text view.
+        final TextView timeView
+                = (TextView)findViewById(
+                R.id.time_view);
+
+        // Creates a new Handler
+        final Handler handler
+                = new Handler();
+
+        handler.post(new Runnable() {
+            @Override
+
+            public void run()
+            {
+                int hours = seconds / 3600;
+                int minutes = (seconds % 3600) / 60;
+                int secs = seconds % 60;
+
+                String time
+                        = String
+                        .format(Locale.getDefault(),
+                                "%d:%02d:%02d", hours,
+                                minutes, secs);
+
+
+                timeView.setText(time);
+
+                if (running) {
+                    seconds++;
+                }
+
+                handler.postDelayed(this, 1000);
+            }
+        });
+    }
+}
